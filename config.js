d6bd843d251e48cf10ddf9c1997b2b2ac14949d0
Bo Zulonas <bozulonas@Patricias-MacBook-Air.local>
2026-07-21 12:07:24 -0700

    Configure Google OAuth client

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Copy this value from the OAuth 2.0 Web application you create in Google Cloud.
 // This file is deliberately local to the app: there is no server or secret involved.
 window.DRIVE_MARKDOWN_CONFIG = {
-  clientId: "PASTE_YOUR_GOOGLE_OAUTH_CLIENT_ID_HERE.apps.googleusercontent.com",
+  clientId: "62785755518-n0o5t8npv72886hv79nfi7ul60oat90o.apps.googleusercontent.com",
   developerKey: "", // Optional, only needed for the Google Drive Picker button.
 };
